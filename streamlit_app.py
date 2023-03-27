8f562e0bc81c65c59a8d0a5349cfb1d08528959e
Will Misenheimer <41021787+willmisenheimer@users.noreply.github.com>
2023-03-27 16:59:50 -0400

    Update streamlit_app.py
    moved code to set_index() back to just after the pick list code

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,11 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 
