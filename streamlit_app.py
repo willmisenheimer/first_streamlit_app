22649a2671f953c64db59d72742aca425ff3727a
Will Misenheimer <41021787+willmisenheimer@users.noreply.github.com>
2023-04-04 15:33:45 -0400

    re-ordering code

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,17 +28,20 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-# takes the json version of the response and normalizes it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# output to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+    # takes the json version of the response and normalizes it
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # output to the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
