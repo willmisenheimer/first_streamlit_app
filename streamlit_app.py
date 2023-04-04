1393e664ca81046fc7a11f44ff35de85083d7eb5
Will Misenheimer <41021787+willmisenheimer@users.noreply.github.com>
2023-04-04 15:02:26 -0400

    get all rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,9 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.fetchall()
+my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
