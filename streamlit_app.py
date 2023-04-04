a8895111af13f27795cd0c2215a679e41ba40f54
Will Misenheimer <41021787+willmisenheimer@users.noreply.github.com>
2023-04-04 15:43:46 -0400

    formatting connector section of code

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,13 +51,19 @@ except URLError as e:
 streamlit.stop()
 
 # snowflake connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+    
+# add a button to load the fruit
+if streamlit.utton('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
